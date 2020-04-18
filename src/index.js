77106f9322fde817130eb5555b6ade87c03d3570
Raymond Hill <infamous.rj@gmail.com>
2020-04-18 14:01:10 -0400

    Smashing semicolons for Gene's sanity <3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const port = process.env.PORT || 3000;
+require('dotenv').config()
+const port = process.env.PORT || 3000
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const promPrefix = 'bean_counter_';
-const promBundle = require("express-prom-bundle");
+const promPrefix = 'bean_counter_'
+const promBundle = require("express-prom-bundle")
 const metricsMiddleware = promBundle({
   includeMethod: true,
   includePath: true,
@@ -16,24 +16,24 @@ const metricsMiddleware = promBundle({
   }
 });
 
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
 // we need to run this from the point of view of where npm is running
-const swaggerDocument = YAML.load('reference/Bean-Counter.v1.yaml');
+const swaggerDocument = YAML.load('reference/Bean-Counter.v1.yaml')
 
-const app = express();
+const app = express()
 const api = require('./api')
 
 // register Prometheus metrics collection for all routes
 // ... except those starting with /api-docs
-app.use("/((?!api-docs))*", metricsMiddleware);
+app.use("/((?!api-docs))*", metricsMiddleware)
 
 // parse requests
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/api', api)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // default route
 app.get('/', (req, res) => {
@@ -42,5 +42,5 @@ app.get('/', (req, res) => {
 
 // listen on port 3000 by default
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`)
 });
